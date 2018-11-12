11031336aa71357c685876ef42180d7010017d53
janaleible <jana.leible@web.de>
2018-11-12 09:38:52 +0100

    add dataset class

--- constants.py
+++ constants.py
@@ -5,3 +5,7 @@ PREPROCESSED_DATA_DIR = os.path.join('.', 'data', 'preprocessed')
 INDEX_FILE = os.path.join('.', 'data', 'index.tar')
 
 PREPROCESSED_DB = os.path.join('data', 'preprocessed.sqlite')
+
+TRAINING_SET = os.path.join('data', 'hotpot', 'train.json')
+DEV_DISTRACTOR_SET = os.path.join('data', 'hotpot', 'dev_distractor.json')
+DEV_FULLWIKI_SET = os.path.join('data', 'hotpot', 'dev_fullwiki.json')
---
+++ dataset/MyDataset.py
@@ -0,0 +1,62 @@
+import json
+from typing import List
+
+from constants import *
+
+
+class Question:
+
+    def __init__(self, question: str, answer: str, type: str, level: str, context: List[List], supporting_facts: List[List]) -> None:
+
+        self.question = question
+        self.answer = answer
+        self.level = level
+        self.type = type
+
+        self.gold_articles = [fact[0] for fact in supporting_facts]
+        self.context = {article[0]: article[1] for article in context}
+
+
+class MyDataset:
+
+    def __init__(self, filename: str, max_questions: int = None) -> None:
+
+        self.questions = []
+        self._current_index = 0
+
+        with open(filename, 'r') as file:
+            json_data = json.load(file)
+
+        for json_question in json_data:
+
+            if max_questions is not None and len(self.questions) > max_questions:
+                break
+
+            question = Question(
+                json_question['question'],
+                json_question['answer'],
+                json_question['type'],
+                json_question['level'],
+                json_question['context'],
+                json_question['supporting_facts']
+            )
+            self.questions.append(question)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        if self._current_index >= len(self.questions):
+            raise StopIteration
+
+        self._current_index += 1
+        return self.questions[self._current_index - 1]
+
+
+if __name__ == '__main__':
+
+    training_set = MyDataset(TRAINING_SET, max_questions=50)
+
+    for question in training_set:
+        print(question.question)
