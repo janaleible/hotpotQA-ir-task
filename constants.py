5e7896278c6ff49a2ea2e92628e439c34231b8c3
janaleible <jana.leible@web.de>
2018-11-17 13:11:38 +0100

    add pytrec_eval evaluation

--- constants.py
+++ constants.py
@@ -21,6 +21,14 @@ TRECEVAL_REFERENCE_DIR = os.path.join('data', 'trec_eval', 'reference')
 TRECEVAL_REFERENCE_TRAIN = os.path.join('data', 'trec_eval', 'reference', 'train_reference.json')
 TRECEVAL_REFERENCE_DEV = os.path.join('data', 'trec_eval', 'reference', 'dev_reference.json')
 
+TRECEVAL_RESULTS_DIR = os.path.join('data', 'trec_eval', 'results')
+TRECEVAL_RESULTS_TRAIN = os.path.join('data', 'trec_eval', 'results', 'train_results.json')
+TRECEVAL_RESULTS_DEV = os.path.join('data', 'trec_eval', 'results', 'dev_results.json')
+
+TRECEVAL_EVALUATION_DIR = os.path.join('data', 'trec_eval', 'evaluation')
+TRECEVAL_EVALUATION_TRAIN = os.path.join('data', 'trec_eval', 'evaluation', 'train_evaluation.json')
+TRECEVAL_EVALUATION_DEV = os.path.join('data', 'trec_eval', 'evaluation', 'dev_evaluation.json')
+
 FILTERED_DIR = os.path.join('data', 'filtered')
 FILTERED_DB = os.path.join('data', 'filtered', 'db.sqlite')
 FILTER_RESULTS = os.path.join('data', 'filtered', 'results.npy')
