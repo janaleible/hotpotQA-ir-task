515cc69a80e9b3d8c8a9bda01d2357015c5a39f9
andrei sili <andrei.sili1994@gmail.com>
2018-11-18 03:48:24 +0100

    Miscellaneous.

--- services/parallel.py
+++ services/parallel.py
@@ -6,11 +6,12 @@ from typing import Callable, Iterable
 def chunk(n, iterable):
     """Collect data into fixed-length chunks or blocks
             grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-        """
+    """
     args = [iter(iterable)] * n
     return itertools.zip_longest(*args, fillvalue=None)
 
 
 def execute(func: Callable, items: Iterable):
+    """Execute a callable over the iterable in parallel."""
     with concurrent.futures.ProcessPoolExecutor() as executor:
         return executor.map(func, items)
