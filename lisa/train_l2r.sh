0527b0fc607784efd606b8a8166284b29e2c8a29
janaleible <jana.leible@web.de>
2018-11-29 11:30:17 +0100

    add lisa script

---
+++ lisa/train_l2r.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Specification of the job requirements for the batch system (number of nodes, expected runtime, etc)
+#SBATCH --job-name=example
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=60000M
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+#Load of modules needed to run your application
+module load Python/3.6.3-foss-2017b
+module load cuDNN/7.0.5-CUDA-9.0.176
+module load NCCL/2.0.5-CUDA-9.0.176
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.4
+4-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
+
+#Preparing input data (e.g. copying input data from your home folder to scratch, preprocessing)
+cp -r $HOME/hotpotQA-ir-task/data/l2r "$TMPDIR"/data/l2r
+
+#Running your application
+cd $HOME/hotpotQA-ir-task
+srun python3 main_l2r.py -a train -e 15 # check python dir
+
+
+#Aggergating output data (e.g. post-processing, copying data from scratch to your home)
+mkdir $HOME/hotpotQA-ir-task/models
+cp -r $TMPDIR/models $HOME/hotpotQA-ir-taks/models
+
