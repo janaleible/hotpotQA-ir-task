41e3d84f83fa94d5a1ca98ae9959c3eb5c772127
janaleible <jana.leible@web.de>
2018-12-19 20:39:56 +0100

    candidate evaluation

---
+++ candidate_db_evaluation.py
@@ -0,0 +1,35 @@
+import json
+import sqlite3
+
+import pytrec_eval
+from tqdm import tqdm
+
+import main_constants as ct
+from services.evaluator import Evaluator
+from services.run import Run
+
+connection = sqlite3.connect(ct.DEV_CANDIDATES_DB)
+cursor = connection.cursor()
+
+question_ids = cursor.execute("SELECT DISTINCT question_id FROM candidates").fetchall()
+run = Run()
+
+for question_id_tuple in tqdm(question_ids):
+
+    question_id = question_id_tuple[0]
+
+    raw_ranking = cursor.execute("select doc_title, tfidf from candidates where question_id = ? order by cast(tfidf as number) desc", [question_id]).fetchall()
+
+    ranking = {json.loads(doc_title): float(pseudo_rank) for ((doc_title, tfidf), pseudo_rank) in zip(raw_ranking, reversed(range(len(raw_ranking))))}
+
+    run.add_ranking(json.loads(question_id), ranking)
+
+evaluator = Evaluator(ct.DEV_TREC_REFERENCE, pytrec_eval.supported_measures)
+
+(_, evaluation) = evaluator.evaluate(run, save=False)
+
+print('ndcg10', evaluation['ndcg_cut_10'])
+print('map10', evaluation['map_cut_10'])
+print('recall10', evaluation['recall_10'])
+print('recall100', evaluation['recall_100'])
+print('recall1000', evaluation['recall_1000'])
